65889033629c2da6fc5aa6b7b5b8bbc4ca260ce9
Antonio45 <40401851+Antonio830@users.noreply.github.com>
2019-02-07 21:33:26 -0600

    cowsay test

---
+++ index.js
@@ -0,0 +1,12 @@
+//var cowsay =require('cowsay');
+
+console.log('hola');
+console.log('soy un archivo');
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+}));
